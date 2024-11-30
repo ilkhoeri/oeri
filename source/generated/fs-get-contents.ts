09b28115fc4da2f0dc24fb342c6a6c22d9057de7
ilkhoeri <khoeriilham@gmail.com>
2024-11-30 23:20:39 +0700

    Update fs-get-contents.ts

--- source/generated/fs-get-contents.ts
+++ source/generated/fs-get-contents.ts
@@ -81,10 +81,16 @@ export async function getContent(
         // Continue to the next extension if file is not found
       }
     }
-    return { content: null, extension: null }; // If none of the extensions matched
+    return {
+      content: await getRepo(`${git_raw}${basePath}`, ".tsx"),
+      extension: ".tsx"
+    }; // If none of the extensions matched
   } catch (error: any) {
     log(error);
-    return { content: null, extension: null };
+    return {
+      content: await getRepo(`${git_raw}${basePath}`, ".tsx"),
+      extension: ".tsx"
+    };
   }
 }
 
